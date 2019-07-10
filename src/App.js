8fe5cd974ba89236ab20cbcb03ceac6744ddac93
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-07-10 17:53:14 -0400

    Have called in data from api. have data displaying. working on getting img to display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+
+
+    useEffect(() => {
+      axios
+        .get(
+          "https://api.nasa.gov/planetary/apod?api_key=aFOyrCyQRJGCrJdSTEO4k23Vxmtk0k0kGQdO55P9"
+        )
+        .then(res => setNasaData(res.data))
+        .catch(error => console.log(error));
+    }, []);
+
+    console.log("img", nasaData)
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        <>
+          <h1>{nasaData.title}</h1>
+          <img src={`${nasaData.url}`} />
+          <h3>NASA explanation: {nasaData.explanation}</h3>
+          <h4>Copyright: {nasaData.copyright}</h4>
+        </>
     </div>
   );
 }
